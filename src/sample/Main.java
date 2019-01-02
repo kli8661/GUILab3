65937fd369adf8dfe89e33c508dc89d0d12477a9
BT_1N3_15 <kli8661@bths.edu>
2019-01-02 09:40:58 -0500

    yeet

---
+++ src/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+       try {
+           this.primaryStage = primaryStage;
+           primaryStage.setTitle("Simon Says");
+           FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+           rootLayout = (AnchorPane) loader.load();
+           Scene scene = new Scene(rootLayout);
+           primaryStage.setScene(scene);
+           primaryStage.show();
+       }
+       catch(Exception e)
+       {
+           e.printStackTrace();
+       }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
