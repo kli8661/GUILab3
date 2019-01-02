22b4d950ed7b922f781b6af7854f81a0f9f64c2c
BT_1N3_15 <kli8661@bths.edu>
2019-01-02 10:09:50 -0500

    Working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,31 @@
 package sample;
 
-import java.util.ArrayList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class Controller {
+import java.awt.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     private GameCode simonsays = new GameCode();
     private ArrayList<String> sequence = simonsays.getSequence();
+
+    @FXML
+    private Rectangle greenBtn;
+
+    @FXML
+    private Rectangle redBtn;
+
+    @FXML
+    private Rectangle yellowbtn;
+
+    @FXML
+    private Rectangle blueBtn;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
 }
