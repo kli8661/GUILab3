b48c11cbab0b4356f5e22bb64e30ef376af6dd97
BT_1N3_15 <kli8661@bths.edu>
2019-01-02 09:46:59 -0500

    Working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,8 @@
 package sample;
 
+import java.util.ArrayList;
+
 public class Controller {
+    private GameCode simonsays = new GameCode();
+    private ArrayList<String> sequence = simonsays.getSequence();
 }
--- src/sample/GameCode.java
+++ src/sample/GameCode.java
@@ -10,4 +10,20 @@ public class GameCode {
     private ArrayList<String> sequence;
     private final String[] COLOR = {"g", "r", "y", "b"};
     private boolean gameRunning;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public ArrayList<String> getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(ArrayList<String> sequence) {
+        this.sequence = sequence;
+    }
 }
