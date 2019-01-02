fab9f3588a3e8636e65e37c197a005db9d8ae7ff
BT_1N3_15 <kli8661@bths.edu>
2019-01-02 09:44:24 -0500

    Working

--- src/sample/GameCode.java
+++ src/sample/GameCode.java
@@ -1,4 +1,13 @@
 package sample;
 
+import java.lang.Math;
+import java.util.ArrayList;
+
 public class GameCode {
+
+    private int index;
+    private final int STARTTURN = 3;
+    private ArrayList<String> sequence;
+    private final String[] COLOR = {"g", "r", "y", "b"};
+    private boolean gameRunning;
 }
